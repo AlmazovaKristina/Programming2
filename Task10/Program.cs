af49dbf073262dea841c110fe9d3f1a323c2b486
Almazova Kristina <cristinaalmaz1702@gmail.com>
2022-09-25 20:06:48 +0300

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Digit( int num)
+
+{
+if (num < 1000 && num > 99)
+{
+     int ThirdDigit = num / 10;
+     int SecondDigit = ThirdDigit % 10;
+     return SecondDigit;
+}
+  else
+ {
+  return 0;
+ }
+}
+int bigresult = Digit (num);
+string result = bigresult > 0 ? $"Цифра {bigresult.ToString()}" :"Введены некоррекные данные";
+Console.WriteLine (result);
