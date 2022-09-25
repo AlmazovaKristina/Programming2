b4180a51a6fad645c6c296c8ed522c301564d6cb
Almazova Kristina <cristinaalmaz1702@gmail.com>
2022-09-25 21:19:02 +0300

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int Digit( int num)
+{
+int i=0;  
+if (num > 99)
+{
+    int ThirdDigit = num;   
+    while (num > 999)
+    {
+        ThirdDigit = num / 10;
+        num = ThirdDigit;
+        i++;
+    }
+        int SecondDigit = ThirdDigit % 10;
+        return SecondDigit;
+}
+//    else
+//  {
+  return 0;
+//  }
+}
+int bigresult = Digit (num);
+string result = bigresult > 0 ? $"Цифра {bigresult.ToString()}" :"Третьей цифры нет";
+Console.WriteLine (result);
