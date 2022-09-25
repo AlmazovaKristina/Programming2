a891f00419aa945b3630258fe23a6257390e8e3a
Almazova Kristina <cristinaalmaz1702@gmail.com>
2022-09-25 22:50:16 +0300

    Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите цифру дня недели:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void print(int day) {
+    if (day < 1 || day > 7) {
+        Console.WriteLine("Некорректное значение. Введите значение от 1 до 7.");
+        return;
+    }
+    if (day == 6 || day == 7) {
+        Console.WriteLine("Да");
+    } else {
+        Console.WriteLine("Нет");
+    }
+}
+
+print(num);
